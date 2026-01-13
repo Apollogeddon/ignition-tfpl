6bc8f8d6cc785985aa9200de74b14fe7ec0bfac1
Apollogeddon <dunc1221@proton.me>
2026-01-13 18:52:35 +1030

    feat(idp): add oidc and saml identity provider types
    * Introduce support for OIDC and SAML identity provider configurations
    * Extend the identity provider schema with OIDC and SAML specific attributes
    * Refactor resource logic to use a generic handler for CRUD operations
    * Add unit tests for internal and OIDC identity provider creation

--- internal/provider/resource_identity_provider.go
+++ internal/provider/resource_identity_provider.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/float64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -28,21 +29,44 @@ func NewIdentityProviderResource() resource.Resource {
 
 // IdentityProviderResource defines the resource implementation.
 type IdentityProviderResource struct {
-	client client.IgnitionClient
+	client  client.IgnitionClient
+	generic GenericIgnitionResource[client.IdentityProviderConfig, IdentityProviderResourceModel]
 }
 
 // IdentityProviderResourceModel describes the resource data model.
 type IdentityProviderResourceModel struct {
-	Id                       types.String `tfsdk:"id"`
-	Name                     types.String `tfsdk:"name"`
-	Description              types.String `tfsdk:"description"`
-	Enabled                  types.Bool   `tfsdk:"enabled"`
-	Type                     types.String `tfsdk:"type"`
-	UserSource               types.String `tfsdk:"user_source"`
+	BaseResourceModel
+	Type                     types.String  `tfsdk:"type"`
+	UserSource               types.String  `tfsdk:"user_source"`
 	SessionInactivityTimeout types.Float64 `tfsdk:"session_inactivity_timeout"`
 	SessionExp               types.Float64 `tfsdk:"session_expiration"`
 	RememberMeExp            types.Float64 `tfsdk:"remember_me_expiration"`
-	Signature                types.String `tfsdk:"signature"`
+	ClientId                 types.String  `tfsdk:"client_id"`
+	ClientSecret             types.String  `tfsdk:"client_secret"`
+	ProviderId               types.String  `tfsdk:"provider_id"`
+	AuthorizationEndpoint    types.String  `tfsdk:"authorization_endpoint"`
+	TokenEndpoint            types.String  `tfsdk:"token_endpoint"`
+	JwkEndpoint              types.String  `tfsdk:"jwk_endpoint"`
+	JwkEndpointEnabled       types.Bool    `tfsdk:"jwk_endpoint_enabled"`
+	UserInfoEndpoint         types.String  `tfsdk:"user_info_endpoint"`
+	LogoutEndpoint           types.String  `tfsdk:"logout_endpoint"`
+	// SAML fields
+	IdpEntityId                 types.String      `tfsdk:"idp_entity_id"`
+	SpEntityId                  types.String      `tfsdk:"sp_entity_id"`
+	SpEntityIdEnabled           types.Bool        `tfsdk:"sp_entity_id_enabled"`
+	AcsBinding                  types.String      `tfsdk:"acs_binding"`
+	NameIdFormat                types.String      `tfsdk:"name_id_format"`
+	SsoServiceConfig            *SsoServiceConfig `tfsdk:"sso_service_config"`
+	ForceAuthn                  types.Bool        `tfsdk:"force_authn"`
+	ResponseSignaturesRequired  types.Bool        `tfsdk:"response_signatures_required"`
+	AssertionSignaturesRequired types.Bool        `tfsdk:"assertion_signatures_required"`
+	IdpMetadataUrl              types.String      `tfsdk:"idp_metadata_url"`
+	IdpMetadataUrlEnabled       types.Bool        `tfsdk:"idp_metadata_url_enabled"`
+}
+
+type SsoServiceConfig struct {
+	Uri     types.String `tfsdk:"uri"`
+	Binding types.String `tfsdk:"binding"`
 }
 
 func (r *IdentityProviderResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -77,12 +101,13 @@ func (r *IdentityProviderResource) Schema(ctx context.Context, req resource.Sche
 				Default:     booldefault.StaticBool(true),
 			},
 			"type": schema.StringAttribute{
-				Description: "The type of the identity provider (currently only 'internal' is supported by this resource).",
+				Description: "The type of the identity provider (internal, oidc, saml).",
 				Required:    true,
 				Validators: []validator.String{
-					stringvalidator.OneOf("internal"),
+					stringvalidator.OneOf("internal", "oidc", "saml"),
 				},
 			},
+			// Internal Type Fields
 			"user_source": schema.StringAttribute{
 				Description: "The name of the User Source Profile used to authenticate users (for 'internal' type).",
 				Optional:    true,
@@ -105,6 +130,104 @@ func (r *IdentityProviderResource) Schema(ctx context.Context, req resource.Sche
 				Computed:    true,
 				Default:     float64default.StaticFloat64(0),
 			},
+			// OIDC Type Fields
+			"client_id": schema.StringAttribute{
+				Description: "The client identifier registered within the identity provider.",
+				Optional:    true,
+			},
+			"client_secret": schema.StringAttribute{
+				Description: "The client secret registered within the identity provider.",
+				Optional:    true,
+				Sensitive:   true,
+			},
+			"provider_id": schema.StringAttribute{
+				Description: "The issuer URL of the identity provider.",
+				Optional:    true,
+			},
+			"authorization_endpoint": schema.StringAttribute{
+				Description: "URL of the OP's OAuth 2.0 Authorization Endpoint.",
+				Optional:    true,
+			},
+			"token_endpoint": schema.StringAttribute{
+				Description: "URL of the OP's OAuth 2.0 Token Endpoint.",
+				Optional:    true,
+			},
+			"jwk_endpoint": schema.StringAttribute{
+				Description: "URL of the OP's JSON Web Key Set document.",
+				Optional:    true,
+			},
+			"jwk_endpoint_enabled": schema.BoolAttribute{
+				Description: "If true, then identity provider public keys will be automatically downloaded.",
+				Optional:    true,
+				Computed:    true,
+				Default:     booldefault.StaticBool(true),
+			},
+			"user_info_endpoint": schema.StringAttribute{
+				Description: "URL to retrieve UserInfo claims from the provider.",
+				Optional:    true,
+			},
+			"logout_endpoint": schema.StringAttribute{
+				Description: "URL at the OP to which an RP can perform a redirect to request that the End-User be logged out.",
+				Optional:    true,
+			},
+			// SAML Type Fields
+			"idp_entity_id": schema.StringAttribute{
+				Description: "The Identity Provider's Entity ID.",
+				Optional:    true,
+			},
+			"sp_entity_id": schema.StringAttribute{
+				Description: "The Service Provider's Entity ID.",
+				Optional:    true,
+			},
+			"sp_entity_id_enabled": schema.BoolAttribute{
+				Description: "True if the SP Entity ID setting should be used.",
+				Optional:    true,
+				Computed:    true,
+				Default:     booldefault.StaticBool(false),
+			},
+			"acs_binding": schema.StringAttribute{
+				Description: "The expected binding used by the IdP (HTTP-Redirect, HTTP-POST).",
+				Optional:    true,
+				Computed:    true,
+				Default:     stringdefault.StaticString("urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"),
+			},
+			"name_id_format": schema.StringAttribute{
+				Description: "The expected name ID format.",
+				Optional:    true,
+				Computed:    true,
+				Default:     stringdefault.StaticString("urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"),
+			},
+			"sso_service_config": schema.SingleNestedAttribute{
+				Description: "The Identity Provider's SSO Service Configuration.",
+				Optional:    true,
+				Attributes: map[string]schema.Attribute{
+					"uri":     schema.StringAttribute{Required: true},
+					"binding": schema.StringAttribute{Required: true},
+				},
+			},
+			"force_authn": schema.BoolAttribute{
+				Optional: true,
+				Computed: true,
+				Default:  booldefault.StaticBool(false),
+			},
+			"response_signatures_required": schema.BoolAttribute{
+				Optional: true,
+				Computed: true,
+				Default:  booldefault.StaticBool(true),
+			},
+			"assertion_signatures_required": schema.BoolAttribute{
+				Optional: true,
+				Computed: true,
+				Default:  booldefault.StaticBool(true),
+			},
+			"idp_metadata_url": schema.StringAttribute{
+				Optional: true,
+			},
+			"idp_metadata_url_enabled": schema.BoolAttribute{
+				Optional: true,
+				Computed: true,
+				Default:  booldefault.StaticBool(true),
+			},
 			"signature": schema.StringAttribute{
 				Description: "The signature of the resource.",
 				Computed:    true,
@@ -118,7 +241,7 @@ func (r *IdentityProviderResource) Configure(ctx context.Context, req resource.C
 		return
 	}
 
-	client, ok := req.ProviderData.(client.IgnitionClient)
+	c, ok := req.ProviderData.(client.IgnitionClient)
 	if !ok {
 		resp.Diagnostics.AddError(
 			"Unexpected Resource Configure Type",
@@ -127,148 +250,155 @@ func (r *IdentityProviderResource) Configure(ctx context.Context, req resource.C
 		return
 	}
 
-	r.client = client
+	r.client = c
+	r.generic = GenericIgnitionResource[client.IdentityProviderConfig, IdentityProviderResourceModel]{
+		Client:     c,
+		Handler:    r,
+		CreateFunc: c.CreateIdentityProvider,
+		GetFunc:    c.GetIdentityProvider,
+		UpdateFunc: c.UpdateIdentityProvider,
+		DeleteFunc: c.DeleteIdentityProvider,
+	}
+}
+
+func (r *IdentityProviderResource) MapPlanToClient(ctx context.Context, model *IdentityProviderResourceModel) (client.IdentityProviderConfig, error) {
+	if model.Type.ValueString() == "internal" {
+		internalConfig := client.IdentityProviderInternalConfig{
+			UserSource:               model.UserSource.ValueString(),
+			SessionInactivityTimeout: model.SessionInactivityTimeout.ValueFloat64(),
+			SessionExp:               model.SessionExp.ValueFloat64(),
+			RememberMeExp:            model.RememberMeExp.ValueFloat64(),
+			AuthMethods: []client.IdentityProviderAuthMethod{
+				{
+					Type:   "basic",
+					Config: map[string]any{},
+				},
+			},
+		}
+		return client.IdentityProviderConfig{
+			Type:   "internal",
+			Config: internalConfig,
+		}, nil
+	} else if model.Type.ValueString() == "oidc" {
+		oidcConfig := client.IdentityProviderOidcConfig{
+			ClientId:                   model.ClientId.ValueString(),
+			ProviderId:                 model.ProviderId.ValueString(),
+			AuthorizationEndpoint:      model.AuthorizationEndpoint.ValueString(),
+			TokenEndpoint:              model.TokenEndpoint.ValueString(),
+			JsonWebKeysEndpoint:        model.JwkEndpoint.ValueString(),
+			JsonWebKeysEndpointEnabled: model.JwkEndpointEnabled.ValueBool(),
+			UserInfoEndpoint:           model.UserInfoEndpoint.ValueString(),
+			EndSessionEndpoint:         model.LogoutEndpoint.ValueString(),
+		}
+
+		if !model.ClientSecret.IsNull() {
+			oidcConfig.ClientSecret = &client.IgnitionSecret{
+				Type: "Embedded",
+				Data: model.ClientSecret.ValueString(),
+			}
+		}
+
+		return client.IdentityProviderConfig{
+			Type:   "oidc",
+			Config: oidcConfig,
+		}, nil
+	} else if model.Type.ValueString() == "saml" {
+		samlConfig := client.IdentityProviderSamlConfig{
+			IdpEntityId:                 model.IdpEntityId.ValueString(),
+			SpEntityId:                  model.SpEntityId.ValueString(),
+			SpEntityIdEnabled:           model.SpEntityIdEnabled.ValueBool(),
+			AcsBinding:                  model.AcsBinding.ValueString(),
+			NameIdFormat:                model.NameIdFormat.ValueString(),
+			ForceAuthnEnabled:           model.ForceAuthn.ValueBool(),
+			ResponseSignaturesRequired:  model.ResponseSignaturesRequired.ValueBool(),
+			AssertionSignaturesRequired: model.AssertionSignaturesRequired.ValueBool(),
+			IdpMetadataUrl:              model.IdpMetadataUrl.ValueString(),
+			IdpMetadataUrlEnabled:       model.IdpMetadataUrlEnabled.ValueBool(),
+			SignatureVerifyingCertificates: []string{},
+			SignatureVerifyingKeys:         []any{},
+		}
+
+		if model.SsoServiceConfig != nil {
+			samlConfig.SsoServiceConfig.Uri = model.SsoServiceConfig.Uri.ValueString()
+			samlConfig.SsoServiceConfig.Binding = model.SsoServiceConfig.Binding.ValueString()
+		}
+
+		return client.IdentityProviderConfig{
+			Type:   "saml",
+			Config: samlConfig,
+		}, nil
+	}
+
+	return client.IdentityProviderConfig{}, fmt.Errorf("unsupported identity provider type: %s", model.Type.ValueString())
+}
+
+func (r *IdentityProviderResource) MapClientToState(ctx context.Context, config *client.IdentityProviderConfig, model *IdentityProviderResourceModel) error {
+	model.Type = types.StringValue(config.Type)
+
+	configBytes, _ := json.Marshal(config.Config)
+
+	if config.Type == "internal" {
+		var internalConfig client.IdentityProviderInternalConfig
+		if err := json.Unmarshal(configBytes, &internalConfig); err == nil {
+			model.UserSource = types.StringValue(internalConfig.UserSource)
+			model.SessionInactivityTimeout = types.Float64Value(internalConfig.SessionInactivityTimeout)
+			model.SessionExp = types.Float64Value(internalConfig.SessionExp)
+			model.RememberMeExp = types.Float64Value(internalConfig.RememberMeExp)
+		}
+	} else if config.Type == "oidc" {
+		var oidcConfig client.IdentityProviderOidcConfig
+		if err := json.Unmarshal(configBytes, &oidcConfig); err == nil {
+			model.ClientId = types.StringValue(oidcConfig.ClientId)
+			model.ProviderId = types.StringValue(oidcConfig.ProviderId)
+			model.AuthorizationEndpoint = stringToNullableString(oidcConfig.AuthorizationEndpoint)
+			model.TokenEndpoint = stringToNullableString(oidcConfig.TokenEndpoint)
+			model.JwkEndpoint = stringToNullableString(oidcConfig.JsonWebKeysEndpoint)
+			model.JwkEndpointEnabled = types.BoolValue(oidcConfig.JsonWebKeysEndpointEnabled)
+			model.UserInfoEndpoint = stringToNullableString(oidcConfig.UserInfoEndpoint)
+			model.LogoutEndpoint = stringToNullableString(oidcConfig.EndSessionEndpoint)
+		}
+	} else if config.Type == "saml" {
+		var samlConfig client.IdentityProviderSamlConfig
+		if err := json.Unmarshal(configBytes, &samlConfig); err == nil {
+			model.IdpEntityId = types.StringValue(samlConfig.IdpEntityId)
+			model.SpEntityId = stringToNullableString(samlConfig.SpEntityId)
+			model.SpEntityIdEnabled = types.BoolValue(samlConfig.SpEntityIdEnabled)
+			model.AcsBinding = types.StringValue(samlConfig.AcsBinding)
+			model.NameIdFormat = types.StringValue(samlConfig.NameIdFormat)
+			model.ForceAuthn = types.BoolValue(samlConfig.ForceAuthnEnabled)
+			model.ResponseSignaturesRequired = types.BoolValue(samlConfig.ResponseSignaturesRequired)
+			model.AssertionSignaturesRequired = types.BoolValue(samlConfig.AssertionSignaturesRequired)
+			model.IdpMetadataUrl = stringToNullableString(samlConfig.IdpMetadataUrl)
+			model.IdpMetadataUrlEnabled = types.BoolValue(samlConfig.IdpMetadataUrlEnabled)
+
+			model.SsoServiceConfig = &SsoServiceConfig{
+				Uri:     types.StringValue(samlConfig.SsoServiceConfig.Uri),
+				Binding: types.StringValue(samlConfig.SsoServiceConfig.Binding),
+			}
+		}
+	}
+
+	return nil
 }
 
 func (r *IdentityProviderResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	var data IdentityProviderResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	internalConfig := client.IdentityProviderInternalConfig{
-		UserSource:               data.UserSource.ValueString(),
-		SessionInactivityTimeout: data.SessionInactivityTimeout.ValueFloat64(),
-		SessionExp:               data.SessionExp.ValueFloat64(),
-		RememberMeExp:            data.RememberMeExp.ValueFloat64(),
-		AuthMethods: []client.IdentityProviderAuthMethod{
-			{
-				Type: "basic",
-				Config: map[string]any{},
-			},
-		},
-	}
-
-	config := client.IdentityProviderConfig{
-		Type:   data.Type.ValueString(),
-		Config: internalConfig,
-	}
-
-	res := client.ResourceResponse[client.IdentityProviderConfig]{
-		Name:    data.Name.ValueString(),
-		Enabled: data.Enabled.ValueBool(),
-		Config:  config,
-	}
-
-	if !data.Description.IsNull() {
-		res.Description = data.Description.ValueString()
-	}
-
-	created, err := r.client.CreateIdentityProvider(ctx, res)
-	if err != nil {
-		resp.Diagnostics.AddError("Error creating Identity Provider", err.Error())
-		return
-	}
-
-	data.Signature = types.StringValue(created.Signature)
-	data.Id = types.StringValue(created.Name)
-	
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	r.generic.Create(ctx, req, resp, &data, &data.BaseResourceModel)
 }
 
 func (r *IdentityProviderResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 	var data IdentityProviderResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	res, err := r.client.GetIdentityProvider(ctx, data.Name.ValueString())
-	if err != nil {
-		resp.Diagnostics.AddError("Error reading Identity Provider", err.Error())
-		return
-	}
-
-	data.Signature = types.StringValue(res.Signature)
-	data.Id = types.StringValue(res.Name)
-	data.Enabled = types.BoolValue(res.Enabled)
-	data.Description = types.StringValue(res.Description)
-	data.Type = types.StringValue(res.Config.Type)
-
-	// Unmarshal internal config if applicable
-	if res.Config.Type == "internal" {
-		var internalConfig client.IdentityProviderInternalConfig
-		configBytes, _ := json.Marshal(res.Config.Config)
-		if err := json.Unmarshal(configBytes, &internalConfig); err == nil {
-			data.UserSource = types.StringValue(internalConfig.UserSource)
-			data.SessionInactivityTimeout = types.Float64Value(internalConfig.SessionInactivityTimeout)
-			data.SessionExp = types.Float64Value(internalConfig.SessionExp)
-			data.RememberMeExp = types.Float64Value(internalConfig.RememberMeExp)
-		}
-	}
-
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	r.generic.Read(ctx, req, resp, &data, &data.BaseResourceModel)
 }
 
 func (r *IdentityProviderResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var data IdentityProviderResourceModel
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	internalConfig := client.IdentityProviderInternalConfig{
-		UserSource:               data.UserSource.ValueString(),
-		SessionInactivityTimeout: data.SessionInactivityTimeout.ValueFloat64(),
-		SessionExp:               data.SessionExp.ValueFloat64(),
-		RememberMeExp:            data.RememberMeExp.ValueFloat64(),
-		AuthMethods: []client.IdentityProviderAuthMethod{
-			{
-				Type: "basic",
-				Config: map[string]any{},
-			},
-		},
-	}
-
-	config := client.IdentityProviderConfig{
-		Type:   data.Type.ValueString(),
-		Config: internalConfig,
-	}
-
-	res := client.ResourceResponse[client.IdentityProviderConfig]{
-		Name:      data.Name.ValueString(),
-		Enabled:   data.Enabled.ValueBool(),
-		Signature: data.Signature.ValueString(),
-		Config:    config,
-	}
-
-	if !data.Description.IsNull() {
-		res.Description = data.Description.ValueString()
-	}
-
-	updated, err := r.client.UpdateIdentityProvider(ctx, res)
-	if err != nil {
-		resp.Diagnostics.AddError("Error updating Identity Provider", err.Error())
-		return
-	}
-
-	data.Signature = types.StringValue(updated.Signature)
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	r.generic.Update(ctx, req, resp, &data, &data.BaseResourceModel)
 }
 
 func (r *IdentityProviderResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	var data IdentityProviderResourceModel
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	err := r.client.DeleteIdentityProvider(ctx, data.Name.ValueString(), data.Signature.ValueString())
-	if err != nil {
-		resp.Diagnostics.AddError("Error deleting Identity Provider", err.Error())
-		return
-	}
+	r.generic.Delete(ctx, req, resp, &data, &data.BaseResourceModel)
 }
 
 func (r *IdentityProviderResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
---
+++ internal/provider/resource_identity_provider_unit_test.go
@@ -0,0 +1,146 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/apollogeddon/terraform-provider-ignition/internal/client"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestUnitIdentityProviderResource_Create(t *testing.T) {
+	mockClient := &client.MockClient{
+		CreateIdentityProviderFunc: func(ctx context.Context, item client.ResourceResponse[client.IdentityProviderConfig]) (*client.ResourceResponse[client.IdentityProviderConfig], error) {
+			if item.Name != "unit-test-idp" {
+				return nil, fmt.Errorf("expected name 'unit-test-idp', got '%s'", item.Name)
+			}
+			
+			// Simulate successful creation
+			item.Signature = "mock-signature-idp"
+			return &item, nil
+		},
+		GetIdentityProviderFunc: func(ctx context.Context, name string) (*client.ResourceResponse[client.IdentityProviderConfig], error) {
+			if name != "unit-test-idp" {
+				return nil, fmt.Errorf("not found")
+			}
+			
+			// Return an internal IdP
+			internalConfig := client.IdentityProviderInternalConfig{
+				UserSource: "default",
+				SessionInactivityTimeout: 30,
+				SessionExp: 0,
+				RememberMeExp: 0,
+			}
+			
+			return &client.ResourceResponse[client.IdentityProviderConfig]{
+				Name:      "unit-test-idp",
+				Enabled:   true,
+				Signature: "mock-signature-idp",
+				Config: client.IdentityProviderConfig{
+					Type: "internal",
+					Config: internalConfig,
+				},
+			}, nil
+		},
+		DeleteIdentityProviderFunc: func(ctx context.Context, name, signature string) error {
+			return nil
+		},
+	}
+
+	providerFactories := map[string]func() (tfprotov6.ProviderServer, error){
+		"ignition": providerserver.NewProtocol6WithError(&IgnitionProvider{
+			version: "test",
+			client:  mockClient,
+		}),
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					provider "ignition" {
+						host  = "http://mock-host"
+						token = "mock-token"
+					}
+					resource "ignition_identity_provider" "unit" {
+						name        = "unit-test-idp"
+						type        = "internal"
+						user_source = "default"
+						session_inactivity_timeout = 30
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("ignition_identity_provider.unit", "name", "unit-test-idp"),
+					resource.TestCheckResourceAttr("ignition_identity_provider.unit", "type", "internal"),
+					resource.TestCheckResourceAttr("ignition_identity_provider.unit", "signature", "mock-signature-idp"),
+				),
+			},
+		},
+	})
+}
+
+func TestUnitIdentityProviderResource_OIDC(t *testing.T) {
+	mockClient := &client.MockClient{
+		CreateIdentityProviderFunc: func(ctx context.Context, item client.ResourceResponse[client.IdentityProviderConfig]) (*client.ResourceResponse[client.IdentityProviderConfig], error) {
+			if item.Config.Type != "oidc" {
+				return nil, fmt.Errorf("expected type 'oidc', got '%s'", item.Config.Type)
+			}
+			item.Signature = "mock-signature-oidc"
+			return &item, nil
+		},
+		GetIdentityProviderFunc: func(ctx context.Context, name string) (*client.ResourceResponse[client.IdentityProviderConfig], error) {
+			oidcConfig := client.IdentityProviderOidcConfig{
+				ClientId: "test-client",
+				ProviderId: "https://auth.com",
+				JsonWebKeysEndpointEnabled: true,
+			}
+			return &client.ResourceResponse[client.IdentityProviderConfig]{
+				Name:      name,
+				Enabled:   true,
+				Signature: "mock-signature-oidc",
+				Config: client.IdentityProviderConfig{
+					Type: "oidc",
+					Config: oidcConfig,
+				},
+			}, nil
+		},
+		DeleteIdentityProviderFunc: func(ctx context.Context, name, signature string) error {
+			return nil
+		},
+	}
+
+	providerFactories := map[string]func() (tfprotov6.ProviderServer, error){
+		"ignition": providerserver.NewProtocol6WithError(&IgnitionProvider{
+			version: "test",
+			client:  mockClient,
+		}),
+	}
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					provider "ignition" {
+						host  = "http://mock-host"
+						token = "mock-token"
+					}
+					resource "ignition_identity_provider" "unit_oidc" {
+						name        = "unit-test-oidc"
+						type        = "oidc"
+						client_id   = "test-client"
+						provider_id = "https://auth.com"
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("ignition_identity_provider.unit_oidc", "type", "oidc"),
+					resource.TestCheckResourceAttr("ignition_identity_provider.unit_oidc", "client_id", "test-client"),
+				),
+			},
+		},
+	})
+}
